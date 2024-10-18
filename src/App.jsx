718a1e4d99188b32ef40410f18bb587580ed0b81
Musab Sayyed <way2musabsayyed@gmail.com>
2024-10-18 11:15:54 +0530

    Setup React and tailwind

---
+++ src/App.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function App() {
+  return (
+    <div>
+      <h1 className='text-blue-400 text-4xl text-center'>Prime<span className='text-black'>Estates</span></h1>
+    </div>
+  )
+}
