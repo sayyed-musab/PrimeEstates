7dbe9a8e067bfb71fa7aecb5a1abecb4223c5494
Musab Sayyed <way2musabsayyed@gmail.com>
2024-10-18 12:31:19 +0530

    Connect to Database

--- server/index.js
+++ server/index.js
@@ -1,5 +1,14 @@
 import express from "express";
+import mongoose from "mongoose";
 
+mongoose
+  .connect(process.env.MONGO_URI)
+  .then(() => {
+    console.log("Connected");
+  })
+  .catch((e) => {
+    console.log(e);
+  });
 const app = express();
 
 app.listen(3000, () => {
