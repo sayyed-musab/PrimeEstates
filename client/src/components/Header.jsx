9187283bc10ebaf0b5520a38dff4be83ad5d4839
Musab Sayyed <way2musabsayyed@gmail.com>
2024-10-18 14:11:54 +0530

    Complete Signup Page UI

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -3,12 +3,12 @@ import { Link } from "react-router-dom";
 
 export default function Header() {
   return (
-    <header className="bg-gray-100 p-4">
+    <header className="bg-gray-800 p-4">
       <div className="flex justify-between items-center max-w-6xl mx-auto">
         <Link to="/">
           <h1 className="font-bold md:text-xl flex flex-wrap">
             <span className="text-amber-700">Prime</span>
-            <span className="text-slate-700">Estates</span>
+            <span className="text-slate-200">Estates</span>
           </h1>
         </Link>
         <form className="bg-white p-1 text-sm md:text-base rounded-lg flex items-center">
@@ -20,13 +20,13 @@ export default function Header() {
           <FaSearch className="text-slate-700" />
         </form>
         <ul className="flex gap-4 text-sm md:text-base">
-          <li className="hidden sm:inline text-smabe-900  hover:border-b-2 hover:border-b-amber-700">
+          <li className="hidden sm:inline text-slate-200  hover:border-b-2 hover:border-b-amber-700">
             <Link to="/">Home</Link>
           </li>
-          <li className="hidden sm:inline text-slate-900  hover:border-b-2 hover:border-b-amber-700">
+          <li className="hidden sm:inline text-slate-200  hover:border-b-2 hover:border-b-amber-700">
             <Link to="/about">About</Link>
           </li>
-          <li className="text-slate-900 hover:border-b-2 hover:border-b-amber-700">
+          <li className="text-slate-200 hover:border-b-2 hover:border-b-amber-700">
             <Link to="/signin">Sign in</Link>
           </li>
         </ul>
