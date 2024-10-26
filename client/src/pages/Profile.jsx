4b6c633f40d8eff2f98ad49a8c5a6152a2c09478
Musab Sayyed <way2musabsayyed@gmail.com>
2024-10-26 10:11:51 +0530

    Add Signout Functionality

--- client/src/pages/Profile.jsx
+++ client/src/pages/Profile.jsx
@@ -14,6 +14,7 @@ import {
   deleteUserFailure,
   deleteUserStart,
   deleteUserSuccess,
+  signOutUserStart,
 } from "../redux/user/userSlice";
 import { useDispatch } from "react-redux";
 
@@ -107,6 +108,22 @@ export default function Profile() {
       dispatch(deleteUserFailure(error.message));
     }
   };
+
+  const handleSignOut = async () => {
+    try {
+      dispatch(signOutUserStart());
+      const res = await fetch("/api/auth/signout");
+      const data = await res.json();
+      if (data.success === false) {
+        dispatch(deleteUserFailure(data.message));
+        return;
+      }
+      dispatch(deleteUserSuccess(data));
+    } catch (error) {
+      dispatch(deleteUserFailure(data.message));
+    }
+  };
+
   return (
     <div className="p-3 max-w-lg mx-auto">
       <h1 className="text-3xl font-semibold text-center my-7">Profile</h1>
@@ -178,7 +195,10 @@ export default function Profile() {
         >
           Delete Account
         </button>
-        <button className="bg-red-700 text-sm text-white p-3 rounded-lg hover:opacity-95">
+        <button
+          onClick={handleSignOut}
+          className="bg-red-700 text-sm text-white p-3 rounded-lg hover:opacity-95"
+        >
           Sign Out
         </button>
       </div>
