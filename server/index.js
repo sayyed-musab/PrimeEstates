0b7fdd445311847cbbd837c47a5d81440e09d9dc
Musab Sayyed <way2musabsayyed@gmail.com>
2024-10-28 12:05:34 +0530

    Add Pre Installation

--- server/index.js
+++ server/index.js
@@ -4,6 +4,7 @@ import userRouter from "./routes/user.route.js";
 import authRouter from "./routes/auth.route.js";
 import listingRouter from "./routes/listing.route.js";
 import cookieParser from "cookie-parser";
+import path from "path";
 
 // Connect to databse
 mongoose
@@ -14,16 +15,20 @@ mongoose
   .catch((e) => {
     console.log(e);
   });
-
+const __dirname = path.resolve();
 const app = express();
 
 app.use(express.json());
 app.use(cookieParser());
-
 // Router
 app.use("/api/user", userRouter);
 app.use("/api/auth", authRouter);
 app.use("/api/listing", listingRouter);
+
+app.use(express.static(path.join(__dirname, "/client/dist")));
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "client", "dist", "index.html"));
+});
 
 app.use((err, req, res, next) => {
   const statusCode = err.statusCode || 500;
