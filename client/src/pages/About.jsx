d93e0b3ea62b0e2c66db7f367c4c2bd8a60e32ba
Musab Sayyed <way2musabsayyed@gmail.com>
2024-10-28 11:51:47 +0530

    Complete About Page

--- client/src/pages/About.jsx
+++ client/src/pages/About.jsx
@@ -1,9 +1,34 @@
-import React from 'react'
+import React from "react";
 
 export default function About() {
   return (
-    <div>
-      About
+    <div className="py-20 px-4 max-w-6xl mx-auto">
+      <h1 className="text-3xl font-bold mb-6 text-slate-800">
+        About <span className="text-amber-700">Prime</span>Estates
+      </h1>
+      <p className="mb-4 text-slate-700">
+        At <strong>PrimeEstates</strong>, we are redefining the real estate
+        experience by connecting clients with their dream properties in the best
+        locations. Our dedicated team of professionals combines extensive market
+        knowledge with innovative tools to make the buying, selling, and renting
+        process seamless and enjoyable.
+      </p>
+      <p className="mb-4 text-slate-700">
+        Our mission is simple: to provide unparalleled guidance and personalized
+        service. Whether you are a first-time buyer, an experienced investor, or
+        seeking a rental property, we offer tailored solutions that ensure a
+        smooth and transparent experience at every step.
+      </p>
+      <p className="mb-4 text-slate-700">
+        With years of industry experience, <strong>PrimeEstates</strong> is
+        committed to exceptional service and professionalism. Our passion for
+        real estate drives us to exceed expectations and transform the property
+        journey into an exciting and fulfilling experience for every client.
+      </p>
+      <p className="mb-4 text-slate-700 font-semibold">
+        Join us at <strong>PrimeEstates</strong> and let’s make your real estate
+        dreams a reality!
+      </p>
     </div>
-  )
+  );
 }
